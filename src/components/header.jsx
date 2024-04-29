e68206f37cf06b784b577177911f8b3897b88e33
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-04-29 20:37:03 +0530

    Login page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,9 @@ export default function Nav(){
     return(
         <div>
             <ol className='flex flex-row-reverse'>
-                <Link className="p-4 text-white" to={'/login'}><li>Login</li></Link>
-                <Link className="p-4 text-white" to={'/signup'}><li>sign up</li></Link>
-                <Link className="p-4 text-white" to={'/home'}><li>home</li></Link>
+                <Link className="p-4 text-black" to={'/login'}><li>Login</li></Link>
+                <Link className="p-4 text-black" to={'/signup'}><li>sign up</li></Link>
+                <Link className="p-4 text-black" to={'/home'}><li>home</li></Link>
             </ol>
         </div>
     )
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,25 +1,24 @@
-
+import "./login.css";
 import Nav from "../components/header";
 export default function Login(){
     return(
            <>
            <Nav/>
-            <div className=" flex justify-center items-center h-screen">
-      <div className="bg-white p-8 rounded-md shadow-md">
-        <h2 className="text-2xl font-semibold mb-6 text-center">Login</h2>
-        <form className="space-y-4" action="#" method="POST">
-          <div>
-            <label htmlFor="username" className="block font-medium">Username:</label>
-            <input type="text" id="username" name="username" className="mt-1 px-4 py-2 block w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500" required />
-          </div>
-          <div>
-            <label htmlFor="password" className="block font-medium">Password:</label>
-            <input type="password" id="password" name="password" className="mt-1 px-4 py-2 block w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500" required />
-          </div>
-          <button type="submit" className="w-full bg-blue-500 text-white font-semibold py-2 rounded-md transition duration-300 hover:bg-blue-600">Login</button>
-        </form>
-      </div>
-    </div>
+            <div>
+              <h1>LOGIN</h1>
+              <form >
+                <div>
+                <label htmlFor="username"> Username: </label>
+                <input className="" type="text" placeholder="ENTER USERNAME" id="username"></input>
+                </div>
+                <div>
+                <label htmlFor="password">Password:</label>
+                <input className="" type="password" id="password" placeholder="ENTER PASSWORD"></input>
+                </div>
+                <button type="submit">Login</button>
+
+              </form>
+            </div>
            </>
     )
 }
